88862f0395ee21e143819f51651825009cdc7863
hugo zeidan <hugzeidan@outlook.com>
2026-04-08 12:34:18 +0200

    made more basic test and began the implementation of the vending machine

--- test_vendingmachine.py
+++ test_vendingmachine.py
@@ -1,20 +1,37 @@
-﻿from timeit import default_timer
-
-import pytest
-from coin import *
+﻿import pytest
 from vendingmachine import *
-def test_valid_coin_insert():
+from coin import *
+from product import *
+
+def test_insert_valid_coin():
     v = VendingMachine()
 
     nickel = Coin.create_nickel()
-    v.InsertCoin(nickel)
+    v.insert_coin(nickel)
 
-    s = v.CheckStorage()
+    assert nickel in v.storage()
+
+def test_insert_invalid_coin():
+    v = VendingMachine()
+
+    penny = Coin.create_penny()
+    v.insert_coin(penny)
+
+    assert penny not in v.storage()
+    assert penny in v.basket()
+    assert len(v.storage()) == 0
+
+def test_insert_multiple_coins():
+    v = VendingMachine()
+
+    nickel = Coin.create_nickel()
+    nickel2 = Coin.create_nickel()
+    dime = Coin.create_dime()
+
+    v.insert_coin(nickel, nickel2, dime)
+
+    s = v.storage()
 
     assert nickel in s
-
-def test_invalid_coin_insert():
-    v = VendingMachine()
-    v.InsertCoin(CoinType.Penny)
-
-    assert CoinType.Penny not in v.CheckStorage()
+    assert nickel2 in s
+    assert dime in s
--- vendingmachine.py
+++ vendingmachine.py
@@ -1,2 +1,16 @@
-﻿class VendingMachine:
-    pass
+﻿from coin import Coin, CoinType
+from typing import List
+
+class VendingMachine:
+    def __init__(self):
+        self._storage:List[Coin] = []
+
+    def storage(self):
+        return self._storage
+
+    def basket(self):
+        pass
+
+    def insert_coin(self, first_coin, *args):
+        coins = list(args) + [first_coin]
+        self._storage.extend(coins)
