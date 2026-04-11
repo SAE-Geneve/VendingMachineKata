0b443ad1b650701bd6cab4d1977b124bfadc8bfd
Jemo <103411555+Jemow@users.noreply.github.com>
2026-04-11 13:05:05 +0200

    test: added fixtures for coins

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -2,47 +2,35 @@ import pytest
 from vending_machine import *
 from coin import Coin
 
-def test_coin_insert(vending_machine):
-    dime = Coin.create_coin(CoinType.DIME)
-    vending_machine.insert_coin(dime)
-    assert dime in vending_machine.get_inserted_coins()
+def test_coin_insert(vending_machine, coin_dime):
+    vending_machine.insert_coin(coin_dime)
+    assert coin_dime in vending_machine.get_inserted_coins()
 
-def test_coin_insert_multiple(vending_machine):
-    dime = Coin.create_coin(CoinType.DIME)
-    nickel = Coin.create_coin(CoinType.NICKEL)
-    quarter = Coin.create_coin(CoinType.QUARTER)
+def test_coin_insert_multiple(vending_machine, coin_dime, coin_nickel, coin_quarter):
+    vending_machine.insert_coin(coin_dime, coin_nickel, coin_quarter)
 
-    vending_machine.insert_coin(dime, nickel, quarter)
+    inserted_coins = vending_machine.get_inserted_coins()
 
-    storage = vending_machine.get_inserted_coins()
+    assert coin_dime in inserted_coins
+    assert coin_nickel in inserted_coins
+    assert coin_quarter in inserted_coins
 
-    assert dime in storage
-    assert nickel in storage
-    assert quarter in storage
-
-def test_coin_invalidity(vending_machine):
-    penny = Coin.create_coin(CoinType.PENNY)
-
-    vending_machine.insert_coin(penny)
+def test_coin_invalidity(vending_machine, coin_penny):
+    vending_machine.insert_coin(coin_penny)
 
     assert len(vending_machine.get_inserted_coins()) == 0
 
-def test_return_coins(vending_machine):
-    penny = Coin.create_coin(CoinType.PENNY)
-    dime = Coin.create_coin(CoinType.DIME)
+def test_return_coins(vending_machine, coin_penny, coin_dime):
+    vending_machine.insert_coin(coin_penny, coin_dime)
 
-    vending_machine.insert_coin(penny, dime)
-
-    assert penny, dime in vending_machine.get_inserted_coins()
+    assert coin_penny, coin_dime in vending_machine.get_inserted_coins()
 
     vending_machine.return_coins()
 
     assert len(vending_machine.get_inserted_coins()) == 0
 
-def test_inserted_coins_sum(vending_machine):
-    quarter = Coin.create_coin(CoinType.QUARTER) # 25 cents
-
-    vending_machine.insert_coin(quarter, quarter, quarter, quarter)
+def test_inserted_coins_sum(vending_machine, coin_quarter):
+    vending_machine.insert_coin(coin_quarter, coin_quarter, coin_quarter, coin_quarter) # 25 cents * 4 = 100
 
     assert vending_machine.inserted_coins_sum() == 100
 
@@ -54,3 +42,19 @@ def test_coin_type(type_coin: CoinType):
 @pytest.fixture
 def vending_machine():
     return VendingMachine()
+
+@pytest.fixture
+def coin_penny():
+    return Coin.create_coin(CoinType.PENNY)
+
+@pytest.fixture
+def coin_dime():
+    return Coin.create_coin(CoinType.DIME)
+
+@pytest.fixture
+def coin_nickel():
+    return Coin.create_coin(CoinType.NICKEL)
+
+@pytest.fixture
+def coin_quarter():
+    return Coin.create_coin(CoinType.QUARTER)
