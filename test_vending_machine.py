9b8b9315f6f15fadd27002b8e46275f48ba68a38
Jemo <103411555+Jemow@users.noreply.github.com>
2026-04-14 16:01:19 +0200

    fix: first assert of make changes test

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -117,8 +117,9 @@ def test_select_product_out_of_stock(vending_machine_no_stock, coin_quarter):
 
     assert vending_machine_no_stock.get_storage_sum() == initial_storage_sum
 
-def test_make_changes(vending_machine_sold_out, coin_quarter, coin_nickel, coin_dime, coin_penny):
+def test_make_changes(vending_machine_sold_out, coin_quarter, coin_nickel, coin_dime):
     vending_machine_sold_out.insert_coin(coin_quarter, coin_quarter, coin_quarter, coin_dime, coin_dime, coin_dime) # $1.05
+    vending_machine_sold_out.add_storage(coin_nickel)
 
     assert vending_machine_sold_out.get_storage_sum() == vending_machine_sold_out.inserted_coins_sum() - product_price_map.get(ProductType.COLA)
     assert vending_machine_sold_out.can_afford(ProductType.COLA) == True
